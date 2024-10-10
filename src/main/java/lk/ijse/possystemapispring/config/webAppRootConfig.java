d3281994311fe4d239ec3dc7d1ce68948b311f4d
UvinduV <uvindusanketh470@gmail.com>
2024-10-10 11:45:51 +0530

    add db

--- src/main/java/lk/ijse/possystemapispring/config/webAppRootConfig.java
+++ src/main/java/lk/ijse/possystemapispring/config/webAppRootConfig.java
@@ -1,11 +1,52 @@
 package lk.ijse.possystemapispring.config;
 
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.possystemapispring")
 public class webAppRootConfig {
+
+    @Bean
+    public DataSource dataSource() { //DB configuration
+        var dmds=new DriverManagerDataSource();
+        dmds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dmds.setUrl("jdbc:mysql://localhost:3306/posSystemAPISpring?createDatabaseIfNotExist=true");
+        dmds.setUsername("root");
+        dmds.setPassword("Ijse@1234");
+        return dmds;
+
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() { //ORM configuration
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean(); //entity manager factory bootstrap kara atha.
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("lk/ijse/possystemapispring/entity");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
 
 }
