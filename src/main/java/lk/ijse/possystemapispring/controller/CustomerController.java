5e1fa235ecb14182df2de944ed9ae3dbd4a035a4
UvinduV <uvindusanketh470@gmail.com>
2024-10-10 10:52:15 +0530

    create CustomerService and CustomerServiceImpl

--- src/main/java/lk/ijse/possystemapispring/controller/CustomerController.java
+++ src/main/java/lk/ijse/possystemapispring/controller/CustomerController.java
@@ -1,9 +1,14 @@
 package lk.ijse.possystemapispring.controller;
 
+import lk.ijse.possystemapispring.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/customers")
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
 }
---
+++ src/main/java/lk/ijse/possystemapispring/service/CustomerService.java
@@ -0,0 +1,4 @@
+package lk.ijse.possystemapispring.service;
+
+public interface CustomerService {
+}
