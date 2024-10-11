21639debddcc2a3fa0d252843a7708c5eeae912d
UvinduV <uvindusanketh470@gmail.com>
2024-10-11 22:19:10 +0530

    create item service

---
+++ src/main/java/lk/ijse/possystemapispring/service/Impl/ItemServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.ijse.possystemapispring.service.Impl;
+
+import jakarta.transaction.Transactional;
+import lk.ijse.possystemapispring.service.ItemService;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class ItemServiceImpl implements ItemService {
+}
